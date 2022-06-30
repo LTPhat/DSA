b3e96f59e4a0c321fd990886fdce23e136879a09
LTPhat <102889533+LTPhat@users.noreply.github.com>
2022-06-30 21:48:10 +0700

    master

---
+++ Graph/Graph_Implementation.cpp
@@ -0,0 +1,26 @@
+//Create a graph with these operation function
+// 1) Add vertex (state)
+// 2) Update Vertex (state name)
+// 3) Delete Vertex 
+// 4) Add Edge 
+// 5) Update Edge (weight)
+// 6) Delete Edge
+// 7) Get all neigbors of a vertex
+// 8) Check if 2 vertices are connected/neighbors
+// 9) Search a vertex
+// 10) Print full graph
+
+// Data member of graph:
+
+// Edge Class
+//  int DestinationVextexID
+//  int weight
+//
+//  Vertex Class
+//  int state_id
+//  string state_name
+//  list <edge> edge list
+
+// Graph class
+// vector <vertex> vertices
+
